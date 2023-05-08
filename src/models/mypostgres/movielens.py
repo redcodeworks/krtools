14ce94095bcee1e6d54aec928db3a795beb79f15
Kevin Riley <kp.riley@icloud.com>
2023-05-08 11:36:43 -0700

    Added date handling to ORM

--- src/models/mypostgres/movielens.py
+++ src/models/mypostgres/movielens.py
@@ -1,16 +1,21 @@
 import re
+import datetime
 
 from pydantic import BaseModel
 
 import sqlalchemy as sa
 from pydantic.dataclasses import dataclass
-from sqlalchemy import Column, Integer, String, Numeric, BIGINT, DATE
+from sqlalchemy import Column, Integer, String, Numeric, DateTime, BIGINT
+import sqlalchemy.types as types
 from sqlalchemy.orm import DeclarativeBase, Mapped, validates
 from sqlalchemy.orm import mapped_column as col
 
 
 class Base(DeclarativeBase):
-    pass
+    type_annotation_map = {
+        int: BIGINT,
+        datetime.datetime: DateTime(timezone=True)
+    }
 
 
 # TODO: Add more precise constraints
@@ -20,7 +25,7 @@ class Movie(Base):
     id: Mapped[int] = col(primary_key=True)
     imdb_id: Mapped[str]
     title: Mapped[str]
-    release_date: Mapped[str | None] = col(DATE)
+    release_date: Mapped[datetime.datetime | None]
     adult: Mapped[str]
     belongs_to_collection: Mapped[str | None]
     budget: Mapped[int]
@@ -29,18 +34,18 @@ class Movie(Base):
     original_language: Mapped[str]
     original_title: Mapped[str]
     overview: Mapped[str]
-    popularity: Mapped[float]
+    popularity: Mapped[float | None]
     poster_path: Mapped[str]
     production_companies: Mapped[str]
     production_countries: Mapped[str]
-    revenue: Mapped[int] = col(BIGINT)
+    revenue: Mapped[int | None]
     runtime: Mapped[float | None]
     spoken_languages: Mapped[str]
     status: Mapped[str]
     tagline: Mapped[str | None]
     video: Mapped[str]
-    vote_average: Mapped[float]
-    vote_count: Mapped[int]
+    vote_average: Mapped[float | None]
+    vote_count: Mapped[int | None]
 
     # TODO: Put validator on CSV parsing side.
     @validates("runtime", "budget", "revenue", "popularity", "vote_average", "vote_count")
@@ -50,10 +55,13 @@ class Movie(Base):
     @validates("release_date")
     def _correct_date_format(self, key, value):
         date_pattern = "^\d{4}\-(0?[1-9]|1[012])\-(0?[1-9]|[12][0-9]|3[01])$"
+        # date_pattern = "%Y-%m-%d"
         if not value:
             return None
-        elif re.match(date_pattern, value):
+        elif isinstance(value, datetime.datetime):
             return value
+        elif re.match(date_pattern, value):
+            return datetime.datetime.strptime(value, "%Y-%m-%d")
         else:
             raise ValueError("Not a valid date format")
 
