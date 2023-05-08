99c8fc510e49e8f706d12ebf85f0e94e2c3ffa7e
Kevin Riley <kevin@redcode.works>
2023-05-08 15:32:02 -0700

    Formatted with black

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,6 @@
 from _pytest.config import Config
 
+
 def pytest_configure(config: Config) -> None:
     """Pytest configuration hook."""
     config.addinivalue_line("markers", "e2e: mark as end-to-end test.")
-
