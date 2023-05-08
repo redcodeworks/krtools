d54a76aeee538fc40dbe6e59e646d0e5c2da7369
Kevin Riley <kevin@redcode.works>
2023-05-08 15:34:13 -0700

    Removed default for sql_alchemy_str

--- src/krtools/conf.py
+++ src/krtools/conf.py
@@ -3,7 +3,7 @@ from pydantic import BaseSettings, SecretStr
 
 class Settings(BaseSettings):
     log_level: str = "WARNING"
-    sql_alchemy_string: SecretStr = "sqlite://"
+    sql_alchemy_string: SecretStr
     read_buffer: int = 64
 
     class Config:
